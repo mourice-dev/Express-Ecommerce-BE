ca7cc1190dbf1144966d562b73fdd58cb19518c5
nshuti <nshutikope@gmail.com>
2026-05-05 23:19:56 +0200

    fix: force cross-site session cookies

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     },
   })
